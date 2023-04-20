faad8296f7408cf95ddd24a85b95a38ab5fb314e
Stennis <91371343+StenniHub@users.noreply.github.com>
2023-04-20 19:04:38 +0200

    Use icon buttons for timer, styling changes and some tweaking

--- src/vue/components/StopWatch.vue
+++ src/vue/components/StopWatch.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="clock">
   
-    <span class="time">{{ this.time }}</span>
-        
+    <span class="time" :class="{ running: this.running, stopped: this.stopped}">{{ this.time }}</span>       
+
     <div class="btn-container">
-      <a @click="this.start">Start</a>
-      <a @click="this.pause">Pause</a>
-      <a @click="this.stop">Stop</a>
+      <icon-button icon="mdi-play" size="x-large" :onClick="start" tooltip="Start timer" :disabled="running" />
+      <icon-button icon="mdi-pause" size="x-large" :onClick="pause" tooltip="Pause timer" :disabled="!running" />
+      <icon-button icon="mdi-stop" size="x-large" :onClick="stop" tooltip="Stop timer" :disabled="stopped || startedAt == null" />
     </div>
   
   </div>
@@ -14,55 +14,62 @@
   
 <script lang="ts">
 import Vue from "vue";
+import IconButton from "./IconButton.vue";
 
 const StopWatch = Vue.extend({
+  components: { IconButton },
   data: (): Record<string, unknown> => ({
     time: "00:00.000",
-    timeBegan: null,
-    timeStopped: null,
-    stoppedDuration: 0,
-    started: null,
-    running: false
+    startedAt: null,
+    pausedAt: null,
+    pausedDuration: 0,
+    interval: null,
+    running: false,
+    stopped: false
   }),
   methods: {
     start(): void {
-      if(this.running) return;
+      if (this.running) return;
             
-      if (this.timeBegan === null) {
+      if (this.startedAt === null) {
         this.reset();
-        this.timeBegan = new Date();
+        this.startedAt = new Date();
       }
 
-      if (this.timeStopped !== null) {
-        this.stoppedDuration += (new Date().valueOf() - this.timeStopped);
+      if (this.pausedAt !== null) {
+        this.pausedDuration += (new Date().valueOf() - this.pausedAt);
       }
 
-      this.started = setInterval(this.clockRunning, 10);	
+      this.interval = setInterval(this.clockRunning, 10);	
       this.running = true;
     },
     pause(): void {
       this.running = false;
-      this.timeStopped = new Date();
-      clearInterval(this.started);
+      this.pausedAt = new Date();
+      clearInterval(this.interval);
     },
     stop(): void {
-      this.running = false;
-      clearInterval(this.started);
-      this.stoppedDuration = 0;
-      this.timeBegan = null;
-      this.timeStopped = null;
+      this.stopped = true;
+      this.pausedDuration = 0;
+      this.startedAt = null;
+      this.pausedAt = null;
+      clearInterval(this.interval);
+
+      if (this.running) {
+        this.running = false;
+      } else {
+        this.reset();
+        return;
+      }
     },
     reset(): void {
-      this.running = false;
-      clearInterval(this.started);
-      this.stoppedDuration = 0;
-      this.timeBegan = null;
-      this.timeStopped = null;
+      if (!this.stopped) this.stop();
+      this.stopped = false;
       this.time = "00:00.000";
     },
     clockRunning(): void {
       var currentTime = new Date().valueOf();
-      var timeElapsed = new Date(currentTime - this.timeBegan - this.stoppedDuration);
+      var timeElapsed = new Date(currentTime - this.startedAt - this.pausedDuration);
       var min = timeElapsed.getUTCMinutes();
       var sec = timeElapsed.getUTCSeconds();
       var ms = timeElapsed.getUTCMilliseconds();
